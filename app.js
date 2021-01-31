d74379a278b0d94e8555d604b3d0dee8f074e1f1
Jacob David C. Cunningham <jcun4128@gmail.com>
2021-01-31 03:32:32 -0600

    current code with basic UI

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config({ path: './.env' });
 const express = require('express');
 const app = express();
 const port = 5000;
+const { getFills } = require('./methods/fills');
+const { processBuySells } = require('./methods/process');
 
 // referenced https://www.digitalocean.com/community/tutorials/how-to-use-ejs-to-template-your-node-application
 // for ejs
@@ -9,8 +11,38 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.render('./pages/index');
+app.get('/', (req, res) => {
+  res.render('./pages/index', { portfolios: {
+    'portfolio 1': 1,
+    'portfolio 2': 2,
+    'portfolio 3': 3
+  }});
+});
+
+app.get('/portfolio-*', (req, res) => {
+  const activePortfolio = req.path.split('-')[1]; // not guaranteed to be right format
+  if (!Number.isInteger(parseInt(activePortfolio))) {
+    res.send('Invalid portfolio number');
+  }
+
+  getFills(activePortfolio).then((data) => {
+    if (data) {
+      res.render('./pages/index', {
+        portfolios: {
+        'portfolio 1': 1,
+        'portfolio 2': 2,
+        'portfolio 3': 3
+        },
+        portfolioData: processBuySells(activePortfolio, data),
+      });
+    } else {
+      res.send('Failed to get data from CBP');
+    }
+  });
+});
+
+app.get('*', (req, res) => {
+  res.send('Invalid route');
 });
 
 app.listen(port, () => {
--- methods/fills.js
+++ methods/fills.js
@@ -2,62 +2,67 @@ require('dotenv').config();
 const crypto = require('crypto');
 const axios = require('axios');
 
-const getFills = (req, res) => {
-  const activePortfolio = 2; // 1, 2, 3 for my case different portfolios see array below.
+const getFills = async (requestedPortfolio) => {
+  return new Promise(resolve => {
+    const activePortfolio = requestedPortfolio;
 
-  const portfolios = [
-    {}, // offset
-    {
-      key: process.env.CBP_DEFAULT_PORTFOLIO_KEY,
-      passphrase: process.env.CBP_DEFAULT_PORTFOLIO_PASSPHRASE,
-      secret: process.env.CBP_DEFAULT_PORTFOLIO_SECRET
-    },
-    {
-      key: process.env.CBP_SHORTS_PORTFOLIO_KEY,
-      passphrase: process.env.CBP_SHORTS_PORTFOLIO_PASSPHRASE,
-      secret: process.env.CBP_SHORTS_PORTFOLIO_SECRET
-    },
-    {
-      key: process.env.CBP_FIFO_PORTFOLIO_KEY,
-      passphrase: process.env.CBP_FIFO_PORTFOLIO_PASSPHRASE,
-      secret: process.env.CBP_FIFO_PORTFOLIO_SECRET
-    }
-  ];
-
-  const secret = portfolios[activePortfolio].secret;
-
-  const timestamp = Date.now() / 1000;
-  const requestPath = '/fills?product_id=BTC-USD';
-
-  const method = 'GET';
-
-  // create the prehash string by concatenating required parts
-  const what = timestamp + method + requestPath;
-
-  // decode the base64 secret
-  const key = new Buffer.from(secret, 'base64');
-
-  // create a sha256 hmac with the secret
-  const hmac = crypto.createHmac('sha256', key);
-
-  // sign the require message with the hmac
-  // and finally base64 encode the result
-  const sign = hmac.update(what).digest('base64');
-
-  axios.get(`${process.env.CBP_API_BASE}/fills?product_id=BTC-USD`, {
-    headers: {
-      'CB-ACCESS-KEY': portfolios[activePortfolio].key,
-      'CB-ACCESS-SIGN': sign,
-      'CB-ACCESS-TIMESTAMP': timestamp,
-      'CB-ACCESS-PASSPHRASE': portfolios[activePortfolio].passphrase
-    }
-  })
+    const portfolios = [
+      {}, // offset
+      {
+        key: process.env.CBP_PORTFOLIO_1_KEY,
+        passphrase: process.env.CBP_PORTFOLIO_1_PASSPHRASE,
+        secret: process.env.CBP_PORTFOLIO_1_SECRET
+      },
+      {
+        key: process.env.CBP_PORTFOLIO_2_KEY,
+        passphrase: process.env.CBP_PORTFOLIO_2_PASSPHRASE,
+        secret: process.env.CBP_PORTFOLIO_2_SECRET
+      },
+      {
+        key: process.env.CBP_PORTFOLIO_3_KEY,
+        passphrase: process.env.CBP_PORTFOLIO_3_PASSPHRASE,
+        secret: process.env.CBP_PORTFOLIO_3_SECRET
+      }
+    ];
+  
+    const secret = portfolios[activePortfolio].secret;
+  
+    const timestamp = Date.now() / 1000;
+    // pagination by &after=2
+    // CB-AFTER header eg. add below
+    // https://docs.pro.coinbase.com/#success
+    const requestPath = '/fills?product_id=BTC-USD';
+  
+    const method = 'GET';
+  
+    // create the prehash string by concatenating required parts
+    const what = timestamp + method + requestPath;
+  
+    // decode the base64 secret
+    const key = new Buffer.from(secret, 'base64');
+  
+    // create a sha256 hmac with the secret
+    const hmac = crypto.createHmac('sha256', key);
+  
+    // sign the require message with the hmac
+    // and finally base64 encode the result
+    const sign = hmac.update(what).digest('base64');
+  
+    axios.get(`${process.env.CBP_API_BASE}/fills?product_id=BTC-USD`, {
+      headers: {
+        'CB-ACCESS-KEY': portfolios[activePortfolio].key,
+        'CB-ACCESS-SIGN': sign,
+        'CB-ACCESS-TIMESTAMP': timestamp,
+        'CB-ACCESS-PASSPHRASE': portfolios[activePortfolio].passphrase
+      }
+    })
     .then(function (response) {
-      res.status(200).send(response.data);
+      resolve(response.data);
     })
     .catch(function (error) {
-      res.status(400).send('error');
+      resolve(false);
     });
+  });
 }
 
 module.exports = {
